8a4422b1c5f2a6e4028a9a2520f91c52f787443b
Kooreis <kooreis.official@gmail.com>
2024-03-01 19:15:29 -0600

    In this update, an array is created to store the prices. The user is then prompted to enter the prices for each day. The Scanner object is closed after all the prices have been entered.

--- solution.java
+++ solution.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of days:");
-        int n = scanner.nextInt();
-    }
+int[] prices = new int[n];
+System.out.println("Enter the prices:");
+for (int i = 0; i < n; i++) {
+    prices[i] = scanner.nextInt();
 }
+scanner.close();
